3d3264d7c515048fb7c97a6b78056283c23fffbc
Matthew Stopa <matthew.p.stopa@gmail.com>
2012-12-17 23:31:32 -0700

    Added license and added examples

--- throttleRequest.js
+++ throttleRequest.js
@@ -1,3 +1,23 @@
+/*
+throttleRequest.js
+Author: Matt Stopa
+MIT license: http://www.opensource.org/licenses/mit-license.php
+
+https://github.com/MattStopa/throttleRequest.js
+http://www.mattstopa.com
+
+Usage:
+  throttleRequest( functionToCall, delayInMilliseconds )
+
+
+Examples:
+  throttleRequest( function() { alert("HI!") }, 1000);
+
+  $("#someElement").on("click", function() {
+    alert("this will only fire if the element has not been clicked on for 5 seconds");
+  }, 5000)
+*/
+
 this.throttleCount = 0;
 
 function throttleRequest(functionToCall, msDelay) {
